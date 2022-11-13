39c644d0bff5541e307d24ae2c7fd22aed41d159
Daniel Dimovski <daniel@timeloop.se>
2022-11-13 16:02:16 +0100

    Always include debug symbols when compiling ooz

--- build.rs
+++ build.rs
@@ -3,6 +3,8 @@ use cmake::Config;
 fn main() {
     let dst = Config::new("ooz")
         .build_target("libooz")
+        // Seg Faults if debug info is not included and crosscompiling
+        .profile("RelWithDebInfo")
         .build();
 
     println!("cargo:rustc-link-search=native={}", format!("{}/build", dst.display()));
